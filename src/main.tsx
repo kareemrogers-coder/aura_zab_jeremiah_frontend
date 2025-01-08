7bf5c0fafba51100d9a1e4638c1beab4219592f2
kareemrogers-coder <kareemrogers27@gmail.com>
2025-01-07 21:03:37 -0500

    commit 6

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Auth0Provider } from '@auth0/auth0-react'
 createRoot(document.getElementById('root')!).render(
   <Auth0Provider
     domain="dev-y870izsbr3wx4epg.us.auth0.com"
-    clientId="uvyewwvDCKGQZifYymYd5axh566HP2zZ"
+    clientId="1x99Sy6tb8brIwwiJXzsoXCqCc3K3htX"
     authorizationParams={{
       redirect_uri: window.location.origin,
       audience: "this-is-our-logo-generator-app-project.",
